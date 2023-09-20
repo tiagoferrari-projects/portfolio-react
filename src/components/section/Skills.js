423ef475ee5835646175876a32a993d4e122f46b
Tiago Ferrari <tiago-projects@outlook.com>
2023-09-20 01:24:10 -0300

    created skills component

--- src/components/section/Skills.js
+++ src/components/section/Skills.js
@@ -1,7 +1,22 @@
+import styles from './Skills.module.css';
+import javascript from '../../img/skills/javascript.svg'
+import html from '../../img/skills/html.svg'
+import css from '../../img/skills/css.svg'
+import react from '../../img/skills/react.svg'
+import typescript from '../../img/skills/typescript.svg'
+
 function Skills(){
     return (
-        <div>
-            Aqui ficará habilidades
+        <div className={styles.skill} id='skill'>
+            <h1>Habilidades</h1>
+            <h4>Conheça um pouco das minhas principais habilidades e conhecimentos.</h4>
+            <div>
+                <img src={javascript}></img>
+                <img src={html}></img>
+                <img src={css}></img>
+                <img src={react}></img>
+                <img src={typescript}></img>
+            </div>
         </div>
     )
 }
