6185ab161cbcefe760593503fa40bad4d07800e0
Tiago Ferrari <tiago-projects@outlook.com>
2023-09-22 01:33:10 -0300

    update footer

--- src/components/section/Footer.js
+++ src/components/section/Footer.js
@@ -1,7 +1,16 @@
-function Footer(){
+import { AiOutlineInstagram, AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
+import styles from './Footer.module.css'
+
+function Footer() {
     return (
-        <div>
-            Aqui ficará rodapé
+        <div className={styles.footer}>
+            <ul>
+                <li><a href='https://www.instagram.com/tiagoferrari_oliveira/' target='_blank'><AiOutlineInstagram size={30} /></a></li>
+                <li><a href='https://github.com/tiagoferrari-projects/' target='_blank'><AiOutlineGithub size={30} /></a></li>
+                <li><a href='https://www.linkedin.com/in/tiago-projects/' target='_blank'><AiOutlineLinkedin size={30} /></a></li>
+            </ul>
+            <p>tiago-projects@outlook.com</p>
+            <p>Tiago Ferrari © 2023</p>
         </div>
     )
 }
